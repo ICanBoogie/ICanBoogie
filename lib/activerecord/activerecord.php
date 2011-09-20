12da0a3f891d3ae8b9d561a3ee6ac23c7b6dc40c
olvlvl <contact@weirdog.com>
2011-09-20 23:18:18 +0200

    Model can now be provided as a string

--- lib/activerecord/activerecord.php
+++ lib/activerecord/activerecord.php
@@ -19,6 +19,7 @@ class ActiveRecord extends Object
 	 * @var Model The model associated with the ActiveRecord.
 	 */
 	protected $_model;
+	protected $_model_id;
 
 	public static function resolve_class_name($module_id, $model_id='primary')
 	{
@@ -38,10 +39,51 @@ class ActiveRecord extends Object
 	 * Constructor.
 	 *
 	 * The constructor function is required when retrieving rows as objects.
+	 *
+	 * @param string|Model $model The model used to store the active record. A model
+	 * object can be provided as well as a model id. If a model id is provied, the model object is
+	 * resolved when the `_model` magic property is accessed.
 	 */
-	public function __construct(Model $model)
+	public function __construct($model)
 	{
-		$this->_model = $model;
+		if (is_string($model))
+		{
+			unset($this->_model);
+			$this->_model_id = $model;
+		}
+		else
+		{
+			$this->_model = $model;
+			unset($this->_model_id);
+		}
+	}
+
+	/**
+	 * Removes the '_model' property before serialization.
+	 *
+	 * @return array
+	 */
+	public function __sleep()
+	{
+		$properties = get_object_vars($this);
+
+		unset($properties['_model']);
+
+		return array_keys($properties);
+	}
+
+	/**
+	 * Returns the model for the active record.
+	 *
+	 * This getter is used when the model has been provided as a string during construct.
+	 *
+	 * @return Model
+	 */
+	protected function __get__model()
+	{
+		global $core;
+
+		return $core->models[$this->_model_id];
 	}
 
 	/**
