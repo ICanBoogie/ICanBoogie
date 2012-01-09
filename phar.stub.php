8b9548b21e2ef058ab9dfaaab3b51bd1768037c7
Olivier Laviale <contact@weirdog.com>
2012-01-09 21:03:51 +0100

    ICanBoogie.php renamed as startup.php

--- phar.stub.php
+++ phar.stub.php
@@ -2,6 +2,6 @@
 
 define('ICanBoogie\ROOT', 'phar://' . __FILE__ . DIRECTORY_SEPARATOR);
 
-require_once ICanBoogie\ROOT . 'ICanBoogie.php';
+require_once ICanBoogie\ROOT . 'startup.php';
 
 __HALT_COMPILER();
