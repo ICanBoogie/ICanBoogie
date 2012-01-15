2b1b33d3e94fbac310c3152d3e36fabc54d0824d
Olivier Laviale <contact@weirdog.com>
2012-01-15 19:32:22 +0100

    Refactored models

--- lib/core/module.php
+++ lib/core/module.php
@@ -404,7 +404,7 @@ class Module extends Object
 
 		$tags += array
 		(
-			Model::T_CLASS => $has_model_class ? Model::resolve_class_name($this->id, $which) : null,
+			Model::T_CLASS => $has_model_class ? Model::resolve_class_name($this->descriptor[self::T_NAMESPACE], $which) : null,
 			Model::T_ACTIVERECORD_CLASS => $has_ar_class ? ActiveRecord::resolve_class_name($this->id, $which) : null,
 			Model::T_NAME => $table_name,
 			Model::T_CONNECTION => 'primary'
