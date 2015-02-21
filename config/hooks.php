44fb67cf07d807d0332b71c1ef3da7edcf498cd5
Olivier Laviale <olivier.laviale@gmail.com>
2015-02-21 13:19:41 +0100

    Removed `routes` getter, which is already defined by the routing package

--- config/hooks.php
+++ config/hooks.php
@@ -2,15 +2,12 @@
 
 namespace ICanBoogie;
 
-$hooks = __NAMESPACE__ . '\Hooks::';
-
 return [
 
 	'prototypes' => [
 
 		'ICanBoogie\Core::get_logger' => 'ICanBoogie\Logger::get_logger',
-		'ICanBoogie\Core::get_session' => 'ICanBoogie\Session::get_session',
-		'ICanBoogie\Core::lazy_get_routes' => $hooks . 'lazy_get_routes'
+		'ICanBoogie\Core::get_session' => 'ICanBoogie\Session::get_session'
 
 	]
 
