ac824a8d9264444afe651bd8c96e6179c56a9bc9
Olivier Laviale <olivier.laviale@gmail.com>
2015-02-22 18:26:03 +0100

    Session code coverage

---
+++ tests/SessionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie;
+
+class SessionTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_exists()
+    {
+        $this->assertFalse(Session::exists());
+    }
+
+	public function test_get_session()
+    {
+        $this->assertInstanceOf('ICanBoogie\Session', Session::get_session(app()));
+    }
+}
