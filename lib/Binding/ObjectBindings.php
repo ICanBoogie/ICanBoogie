2f398daf68b8fa84ab0e3683a0201878adc72a67
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-16 18:06:43 +0200

    Added ObjectBindings

---
+++ lib/Binding/ObjectBindings.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie\Binding;
+
+/**
+ * {@link \ICanBoogie\Object} prototype methods.
+ *
+ * @property-read \ICanBoogie\Core $app
+ */
+trait ObjectBindings
+{
+
+}
