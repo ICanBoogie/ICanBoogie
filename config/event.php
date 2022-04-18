7eb1228d976f01695885f34107ac7afc282ee53d
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-18 23:52:08 +0200

    Use callable notation for events

--- config/event.php
+++ config/event.php
@@ -4,15 +4,8 @@ namespace ICanBoogie;
 
 use ICanBoogie\Application\ClearCacheEvent;
 
-use function ICanBoogie\Event\qualify_type;
-
-$hooks = Hooks::class . '::';
-
-/**
- * @uses Hooks::on_clear_cache()
- */
 return [
 
-	qualify_type(Application::class, ClearCacheEvent::TYPE) => $hooks . 'on_clear_cache'
+	ClearCacheEvent::qualify(Application::class) => [ Hooks::class, 'on_clear_cache' ],
 
 ];
